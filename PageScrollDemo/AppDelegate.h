a0a610d51a802ae827c9affe35f2fb415490584d
Xummer <xummer26@gmail.com>
2013-05-15 17:35:06 +0800

    Initial Commit

---
+++ PageScrollDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PageScrollDemo
+//
+//  Created by Xummer on 13-5-15.
+//  Copyright (c) 2013年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PageScrollDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PageScrollDemo
+//
+//  Created by Xummer on 13-5-15.
+//  Copyright (c) 2013年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
